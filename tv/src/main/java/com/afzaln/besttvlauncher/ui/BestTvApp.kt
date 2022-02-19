6c6b544a425957a2c1d85f889908b9648418051c
Afzal Najam <afzalnaj@gmail.com>
2022-02-19 18:33:44 -0500

    Update Compose and AGP
    Add release config

--- tv/src/main/java/com/afzaln/besttvlauncher/ui/BestTvApp.kt
+++ tv/src/main/java/com/afzaln/besttvlauncher/ui/BestTvApp.kt
@@ -4,23 +4,10 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavDestination
-import androidx.navigation.NavType
-import androidx.navigation.navArgument
-import com.afzaln.besttvlauncher.ui.apps.AppsScreen
-import com.afzaln.besttvlauncher.ui.channels.ChannelsScreen
-import com.afzaln.besttvlauncher.ui.destinations.HomeScreenDestination
-import com.afzaln.besttvlauncher.ui.destinations.ItemDetailsScreenDestination
-import com.afzaln.besttvlauncher.ui.home.HomeScaffold
-import com.afzaln.besttvlauncher.ui.home.HomeScreen
 import com.afzaln.besttvlauncher.ui.theme.AppTheme
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.ramcosta.composedestinations.DestinationsNavHost
-import com.ramcosta.composedestinations.utils.composable
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -30,7 +17,6 @@ fun BestTvApp() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-
             DestinationsNavHost(navGraph = NavGraphs.root)
         }
     }
