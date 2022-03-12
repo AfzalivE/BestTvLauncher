ba7a547f783c0f5f83a202b4b2dc04ef4e4f176d
Afzal Najam <afzalnaj@gmail.com>
2022-03-12 16:06:10 -0500

    Fix app needing restart after TV listings permission is granted

--- tv/src/main/java/com/afzaln/besttvlauncher/HomeActivity.kt
+++ tv/src/main/java/com/afzaln/besttvlauncher/HomeActivity.kt
@@ -1,34 +1,63 @@
 package com.afzaln.besttvlauncher
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.registerForActivityResult
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.afzaln.besttvlauncher.ui.BestTvApp
+import com.afzaln.besttvlauncher.ui.theme.AppTheme
 
 class HomeActivity : ComponentActivity() {
+    lateinit var permissionRequester: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BestTvApp()
-        }
 
-        val permissionRequester =
+        permissionRequester =
             registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
-                if (!granted) {
-                    Toast.makeText(
-                        this,
-                        "Cannot show recommendations without permission",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                if (granted) {
+                    showApp()
+                } else {
+                    showPermissionError()
                 }
             }
 
-        permissionRequester.launch(PERMISSION_READ_TV_LISTINGS)
+        requestPermission()
+    }
+
+    private fun showPermissionError() = setContent {
+        AppTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "The READ_TV_LISTINGS permission is required to get recommendations from installed apps on this TV.")
+                    Spacer(Modifier.height(16.dp))
+                    Button(onClick = {
+                        requestPermission()
+                    }) {
+                        Text(text = "Click here to request permission")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun requestPermission() = permissionRequester.launch(PERMISSION_READ_TV_LISTINGS)
+
+    private fun showApp() = setContent {
+        BestTvApp()
     }
 }
 
